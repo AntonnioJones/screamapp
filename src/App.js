98a6d7f227ae6ec00911742a9c796fece81cb011
Antonnio Jones <antonnioj@gmail.com>
2020-08-24 11:48:33 -0400

    added deatails to navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+//componets
+import Navbar from "./components/Navbar";
 
 //pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
         </Router>
       </div>
     );
