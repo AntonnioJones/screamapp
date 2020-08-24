9d404dae35c46734cdce4e8268dd3eebaa50a709
Antonnio Jones <antonnioj@gmail.com>
2020-08-24 18:41:40 -0400

    finishes scream cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 //componets
 import Navbar from "./components/Navbar";
@@ -10,10 +12,30 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        light: '#f27573',
+        main: '#ef5350',
+        dark: '#a73a38',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: '#6573c3',
+        main: '#3f51b5',
+        dark: '#2c387e',
+        contrastText: '#fff'
+      },
+    },
+  }
+);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
         <Router>
           <Navbar />
           <div className="container">
@@ -25,6 +47,7 @@ class App extends Component {
           </div>
         </Router>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import "../App.css";
+import axios from "axios";
+import Scream from '../components/Scream'
 
 export class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home page</h1>
-            </div>
-        )
-    }
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "/screams"
+      )
+      .then((res) => {
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream,index) => {
+        return <Scream key={index} scream={scream} />
+      })
+    ) : (
+      <p>Loading...</p>
+    );
+
+    return (
+      <Grid container>
+        <Grid item sm={8} xs={12}>
+        {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          content...
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
