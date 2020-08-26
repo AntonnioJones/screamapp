509153cc4295b2174554c25709664cff4c317b22
Antonnio Jones <antonnioj@gmail.com>
2020-08-26 19:25:45 -0400

    working on login form

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -5,6 +5,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 const styles = {
   card: {
@@ -25,6 +27,7 @@ const styles = {
 
 export class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: { body, createdAt, userImage, userHandle },
@@ -46,7 +49,7 @@ export class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import Grid from "@material-ui/core/Grid";
+
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+};
 
 export class login extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Login page</h1>
-            </div>
-        )
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <p>yoooo</p>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
-export default login
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
