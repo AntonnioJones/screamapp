d681dc5b58c32ffb8538be4675fa19509d2ad5de
Antonnio Jones <antonnioj@gmail.com>
2020-08-29 20:20:33 -0400

    almost done with signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 //componets
 import Navbar from "./components/Navbar";
@@ -12,41 +12,40 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-const theme = createMuiTheme(
-  {
-    palette: {
-      primary: {
-        light: '#f27573',
-        main: '#ef5350',
-        dark: '#a73a38',
-        contrastText: '#fff'
-      },
-      secondary: {
-        light: '#6573c3',
-        main: '#3f51b5',
-        dark: '#2c387e',
-        contrastText: '#fff'
-      },
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#f27573",
+      main: "#ef5350",
+      dark: "#a73a38",
+      contrastText: "#fff",
     },
-  }
-);
+    secondary: {
+      light: "#6573c3",
+      main: "#3f51b5",
+      dark: "#2c387e",
+      contrastText: "#fff",
+    },
+  },
+  
+});
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
       </MuiThemeProvider>
     );
   }
--- src/pages/login.js
+++ src/pages/login.js
@@ -59,7 +59,7 @@ export class login extends Component {
     };
     Axios.post("/login", userData)
       .then((res) => {
-        console.log(res.data);
+        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
         this.setState({
           loading: false,
         });
