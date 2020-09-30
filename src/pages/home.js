7e8d844dbd3d82c9cf4527d95a7487d06def8e95
Antonnio Jones <antonnioj@gmail.com>
2020-09-29 20:28:37 -0400

    almost done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,48 +1,48 @@
-import React, { Component } from "react";
-import Grid from "@material-ui/core/Grid";
-import "../App.css";
-import axios from "axios";
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+
 import Scream from '../components/Scream';
 import Profile from '../components/Profile';
 
-export class home extends Component {
-  state = {
-    screams: null,
-  };
+
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
+
+class home extends Component {
   componentDidMount() {
-    axios
-      .get(
-        "/screams"
-      )
-      .then((res) => {
-        this.setState({
-          screams: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    this.props.getScreams();
   }
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => {
-        return <Scream key={scream.screamId} scream={scream} />
-      })
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
     ) : (
-      <p>Loading...</p>
+      <div></div>
     );
-
     return (
       <Grid container>
         <Grid item sm={8} xs={12}>
-        {recentScreamsMarkup}
+          {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <Profile/>
+          <Profile />
         </Grid>
       </Grid>
     );
   }
 }
 
-export default home;
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data
+});
+
+export default connect(
+  mapStateToProps,
+  { getScreams }
+)(home);
